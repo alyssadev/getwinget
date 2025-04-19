eb831e45e7d4feac3b830a1090e5027dfd9d0b0c
Aly Smith <63546997+alyssadev@users.noreply.github.com>
2025-04-19 12:22:52 +1000

    Update getwinget.ps1

--- getwinget.ps1
+++ getwinget.ps1
@@ -15,5 +15,7 @@ if (-NOT ([bool]($cur.Groups -match $adm))) {
 Install-PackageProvider -Name NuGet -Force -ErrorAction SilentlyContinue | Out-Null
 Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
 Repair-WinGetPackageManager
-Import-Module Appx -UseWinPS # fixes Get-AppxPackage being removed from pwsh 7
+if(!(get-command Add-AppxPackage)) {
+  Import-Module Appx -UseWinPS # fixes Get-AppxPackage being removed from pwsh 7
+}
 Add-AppxPackage https://cdn.winget.microsoft.com/cache/source.msix <#
